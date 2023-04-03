962bf0a26e1f26467ccfd991051cb20b19b99420
@Delphine2669 <delphine.thevenet2669@gmail.com>
2023-04-03 17:25:19 +0200

    pokemon card created

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import PokemonCard from './components/PokemonCard'
+import React from 'react'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -28,7 +29,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+        <PokemonCard />
+      </div>
     </div>
+
   )
 }
 
