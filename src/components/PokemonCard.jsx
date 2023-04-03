056ce10e948044d3b5ec5845619677debf52d8d9
@Delphine2669 <delphine.thevenet2669@gmail.com>
2023-04-03 18:52:47 +0200

    react basic 3

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,35 @@
 const PokemonCard = () => {
-    return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="picture of pokemon bulbasaur" />
-            <figcaption>Bulbasaur</figcaption> </figure>
+    const pokemon = pokemonList[0]
+    if (pokemon.imgSrc) {
+
+        return (
+            <figure>
+                <img src={pokemon.imgSrc} alt={`picture of pokemon ${pokemon.name}`} />
+                <figcaption>{pokemon.name}</figcaption> </figure>
+        )
+    }
+    else {
+        return <figure>
+            <p> ??? </p>
+            <figcaption>{pokemon.name}</figcaption> </figure>
 
 
-    )
+    }
 }
+
+
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
+
+
 
 export default PokemonCard
